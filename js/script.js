1548286b095b8b1067fd7635e958d5c6813c03df
Kiseluie <timurkirdakov@gmail.com>
2023-05-07 14:23:03 +0300

    Добавлен маганзин с бустерами

--- js/script.js
+++ js/script.js
@@ -30,54 +30,93 @@ class Booster {
     constructor() {
         this.boosters = {
             a: {
-                cost: 10,
-                value: 0
+                cost: 100,
+                value: 2
             },
             b: {
-                cost: 25,
-                value: 0
+                cost: 1000,
+                value: 5
             },
             c: {
-                cost: 100,
-                value: 0
+                cost: 10000,
+                value: 10
             },
         };
 
         this.pelmen = 0;
         this.money = 0;
+        this.multiplyer = 1;
     }
 
-    buy(boost) {
-        if ( this.boosters.hasOwnProperty(boost) ) {
-            this.boosters[bust].value += 1;
-            this.boosters[bust].cost *= 2;
-        } else {
-            console.log('Error. Unknown bust: ' + boost);
-        }
-    }
+    // buy(boost) {
+    //     if ( this.boosters.hasOwnProperty(boost) ) {
+    //         this.boosters[bust].value += 1;
+    //         this.boosters[bust].cost *= 2;
+    //     } else {
+    //         console.log('Error. Unknown bust: ' + boost);
+    //     }
+    // }
     
     click() {
         this.pelmen += 1;
 
         this.money +=
-            1 +
-            this.boosters.a.value * 1 +
-            this.boosters.b.value * 2 +
-            this.boosters.c.value * 4;
+            1 * this.multiplyer;
+            // this.boosters.a.value * 1 +
+            // this.boosters.b.value * 2 +
+            // this.boosters.c.value * 4;
     }
 
-    getBoost(boost) {
-        if ( this.boosters.hasOwnProperty(boost) ) {
+    selectBoost(){
+        document.querySelector("#first-boost").onclick = () => {
+            if (this.money >= 100){
+                this.moeny -= 100;
+            
+                this.multiplyer = 5;
+                const add_btn = document.querySelector('#first-boost');
+                add_btn.classList.add('none');
+            }
+            else{
+                alert('Недостаточно благословений!');
+            }
+        }
 
-        } else {
-            console.log('Error. Unknown bust: ' + boost);
+        document.querySelector("#second-boost").onclick = () => {
+            if (this.money >= 1000){
+                this.moeny -= 1000;
+           
+                this.multiplyer = 10;
+                const add_btn = document.querySelector('#second-boost');
+                add_btn.classList.add('none');   
+            }
+            else{
+                alert('Недостаточно благословений!');
+            }
+        }
+
+        document.querySelector("#third-boost").onclick = () => {
+            if (this.money >= 10000){
+                this.moeny -= 10000;
+            
+                this.multiplyer += 10;
+            }
+            else{
+                alert('Недостаточно благословений!');
+            }
         }
     }
+    // getBoost(boost) {
+    //     if ( this.boosters.hasOwnProperty(boost) ) {
+
+    //     } else {
+    //         console.log('Error. Unknown bust: ' + boost);
+    //     }
+    // }
 }
 
 class Shop {
     constructor() {
-
+        
     }
 }
 
@@ -102,9 +141,9 @@ class Game {
         this.buttons.count.innerHTML = this.booster.pelmen;
         this.buttons.money.innerHTML = this.booster.money;
         
-        this.buttons.a.innerHTML = this.booster.boosters.a.value;
-        this.buttons.b.innerHTML = this.booster.boosters.b.value;
-        this.buttons.c.innerHTML = this.booster.boosters.c.value;
+        this.buttons.a.innerHTML = this.booster.boosters.a.cost;
+        this.buttons.b.innerHTML = this.booster.boosters.b.cost;
+        this.buttons.c.innerHTML = this.booster.boosters.c.cost;
 
     }
 }
@@ -114,12 +153,20 @@ let game = new Game();
 
 document.querySelector("#Shlepa").onclick = () => {
     game.booster.click();
+    game.booster.selectBoost();
 }
 
 document.querySelector("#Shop").onclick = () => {
     const shop = document.querySelector('.shop');
     shop.classList.remove('none');
 }
+
+document.querySelector("#close").onclick = () => {
+    const shop = document.querySelector('.shop');
+    shop.classList.add('none');
+}
+
+
 
 
 // Отображение счёта независимо от основной игры
